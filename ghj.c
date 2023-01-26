0b622eceb051e1271e88ca0312ccb4b94cd82dc8
turn-sole <113235914+turn-sole@users.noreply.github.com>
2023-01-26 20:37:57 +0800

    Add files via upload

---
+++ ghj.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+//int main()
+//{
+//	char ch = 'w';
+//	char* pc = &ch;
+//	int num = 10;
+//	int* pi = &num;
+//	int arr[10];
+//	int(*pa)[10] = &arr;
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int(*p)[10] = &arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", (*p)[i]);
+//	}
+//	return 0;
+//}
+//void print(int arr[3][4], int r, int j)
+//{
+//	int i = 0;
+//	for (i = 0; i < r; i++)
+//	{
+//		int m = 0;
+//		for (m = 0; m < j; m++)
+//		{
+//			printf("%d ",arr[i][m]);
+//		}
+//		printf("\n");
+//	}
+//}
+//void print(int(*p)[4], int r, int c)
+//{
+//	int i = 0;
+//	for (i = 0; i < r; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < c; j++)
+//		{
+//			printf("%d ", (*(p + i))[j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int arr[3][4] = { {1,2,3,4},{2,3,4,5},{3,4,5,6} };
+//	/*print(arr, 3, 4);*/
+//	print(arr, 3, 4);
+//	return 0;
+//}
+int add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	
+	return 0;
+}
